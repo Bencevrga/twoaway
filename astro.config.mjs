ff47fc525147f2bfb8de11e5c3dbb187d142643e
Bencevrga <2005bencevarga@gmail.com>
2025-11-18 19:16:41 +0100

    Fix Astro base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/Bencevrga/twoaway.git',
-  integrations: [tailwind()]
+  site: 'https://bencevrga.github.io/twoaway/',
+  base: '/twoaway/',
+  integrations: [tailwind()],
 });
